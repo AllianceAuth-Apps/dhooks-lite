7bc16da40b4cd85f1147413e1b8b080e8087ccbe
ErikKalkoken <erik.kalkoken@gmail.com>
2023-07-16 15:41:12 +0200

    Add tests to reach 100% coverage

--- tests/test_serializers.py
+++ tests/test_serializers.py
@@ -2,6 +2,7 @@ import json
 from datetime import datetime
 from unittest import TestCase
 
+from dhooks_lite.embed import Author
 from dhooks_lite.serializers import JsonDateTimeEncoder
 from tests.utils import set_test_logger
 
@@ -10,9 +11,18 @@ logger = set_test_logger(MODULE_PATH, __file__)
 
 
 class TestJsonDateTimeEncoder(TestCase):
-    def test_encode(self):
+    def test_should_encode_datetime_without_error(self):
+        # given
         my_date = datetime(2020, 8, 3, 16, 5, 0)
         my_dict = {"alpha": "dummy", "bravo": my_date}
-
+        # when
         result = json.loads(json.dumps(my_dict, cls=JsonDateTimeEncoder))
+        # then
         self.assertEqual(result, {"alpha": "dummy", "bravo": "2020-08-03T16:05:00"})
+
+    def test_should_raise_error_for_unknown_type(self):
+        # given
+        my_dict = {"unknown": Author("dummy")}
+        # when/then
+        with self.assertRaises(TypeError):
+            json.loads(json.dumps(my_dict, cls=JsonDateTimeEncoder))
